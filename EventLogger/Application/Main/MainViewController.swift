ea4fe71efd3177e10c599e06770a4219b4336072
Jade Lapuz <jhade.lapuz@gmail.com>
2024-11-21 23:29:38 +0800

    [SETUP] - initial commit

---
+++ EventLogger/Application/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventLogger
+//
+//  Created by Jade Lapuz on 11/21/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
